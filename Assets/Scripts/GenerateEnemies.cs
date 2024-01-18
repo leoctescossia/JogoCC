b620c5c9203cd4edb2075fa4eb05b4c39c3c844d
Leonardo Escóssia <111150173+leoctescossia@users.noreply.github.com>
2024-01-18 13:57:47 -0300

    Add Spawn at Mobs

---
+++ Assets/Scripts/GenerateEnemies.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateEnemies : MonoBehaviour
+{
+    public GameObject theEnemy;
+    public int xPos;
+    public int zPos;
+    public int enemyCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+
+    IEnumerator EnemyDrop()
+    {
+        while(enemyCount < 10)
+        {
+            // Ajuste as coordenadas conforme necessário
+            float xPos = 178.89f + Random.Range(-5f, 5f); // Adicionando um deslocamento aleatório para x
+            float y = 0.02f;
+            float zPos = 42f + Random.Range(-5f, 5f); // Adicionando um deslocamento aleatório para z
+
+
+
+            Instantiate(theEnemy, new Vector3(xPos, y, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            enemyCount += 1;
+        }
+    }
+}
